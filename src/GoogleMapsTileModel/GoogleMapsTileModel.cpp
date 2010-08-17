afc663b16fae7ac33a5549c1ca59c805e9b55061
Vladimír Vondruš <mosra@centrum.cz>
2010-08-17 21:05:40 +0200

    Google Maps raster model.

---
+++ src/GoogleMapsTileModel/GoogleMapsTileModel.cpp
@@ -0,0 +1,71 @@
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include "GoogleMapsTileModel.h"
+
+using namespace Map2X::Core;
+using namespace std;
+
+namespace Map2X { namespace Plugins {
+
+vector<Zoom> GoogleMapsTileModel::zoomLevels() const {
+    vector<Zoom> z;
+    for(Zoom i = 0; i != 23; ++i)
+        z.push_back(i);
+    return z;
+}
+
+TileArea GoogleMapsTileModel::area() const {
+    return TileArea(0, 0, 1, 1);
+}
+
+vector<string> GoogleMapsTileModel::layers() const {
+    vector<string> l;
+    l.push_back("Map");
+    l.push_back("Satellite");
+    return l;
+}
+
+vector<string> GoogleMapsTileModel::overlays() const {
+    vector<string> l;
+    l.push_back("Labels");
+    return l;
+}
+
+string GoogleMapsTileModel::tileUrl(const std::string& layer, Zoom z, const Map2X::Core::TileCoords& coords) const {
+    /* "Random" server number */
+    int servernum = (coords.x + 2*coords.y)%4;
+
+    /* Galileo substring */
+    string galileo = string("Galileo").substr(0, (coords.x*3 + coords.y)%8);
+
+    /* URL for given layer */
+    ostringstream url;
+    url << "http://";
+    if(layer == "Map")
+        url << "mt" << servernum << ".google.com/vt/lyrs=m@130";
+    else if(layer == "Satellite")
+        url << "khm" << servernum << ".google.com/kh/v=66";
+    else if(layer == "Labels")
+        url << "mt" << servernum << ".google.com/vt/lyrs=h@130";
+    else return "";
+
+    url << "&x=" << coords.x << "&y=" << coords.y << "&z=" << z << "&s=" << galileo;
+
+    return url.str();
+
+}
+
+}}
---
+++ src/GoogleMapsTileModel/GoogleMapsTileModel.h
@@ -0,0 +1,60 @@
+#ifndef Map2X_Plugins_GoogleMapsTileModel_h
+#define Map2X_Plugins_GoogleMapsTileModel_h
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class Map2X::Plugins::GoogleMapsTileModel
+ */
+
+#include "AbstractMercatorTileModel.h"
+
+namespace Map2X { namespace Plugins {
+
+/** @brief Tile model for Google Maps */
+class GoogleMapsTileModel: public Core::AbstractMercatorTileModel {
+    public:
+        GoogleMapsTileModel(PluginManager::AbstractPluginManager* manager, const std::string& pluginName):
+            AbstractMercatorTileModel(manager, pluginName) {}
+
+        inline virtual int features() const
+            { return Core::AbstractMercatorTileModel::features()|LoadableFromUrl; }
+        inline virtual Core::TileSize tileSize() const
+            { return Core::TileSize(256,256); }
+        virtual std::string copyright() const
+            { return "© 2010 Google, Map Data © 2010 Tele Atlas."; }
+
+        virtual std::vector<Core::Zoom> zoomLevels() const;
+        virtual Core::TileArea area() const;
+        virtual std::vector<std::string> layers() const;
+        virtual std::vector<std::string> overlays() const;
+
+        /**
+         * Server number and Galielo secure string algorithm stolen from
+         * http://www.codeproject.com/KB/scrapbook/googlemap.aspx
+         */
+        virtual std::string tileUrl(const std::string& layer, Core::Zoom z, const Map2X::Core::TileCoords& coords) const;
+};
+
+}}
+
+#ifndef DOXYGEN_GENERATING_OUTPUT
+PLUGIN_REGISTER(Map2X::Plugins::GoogleMapsTileModel,
+                "cz.mosra.Map2X.Core.AbstractTileModel/0.1");
+PLUGIN_SET_NAME("Google Maps");
+PLUGIN_FINISH
+#endif
+
+#endif
